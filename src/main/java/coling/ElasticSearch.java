9105cfd9fd3c98ba1d5705a67bf20f49168adb92
colining <18810782954@163.com>
2018-04-11 18:53:00 +0800

    init

--- src/main/java/coling/ElasticSearch.java
+++ src/main/java/coling/ElasticSearch.java
@@ -1,75 +1,60 @@
 package coling;
-
-
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
-
 import java.io.IOException;
 import java.util.Scanner;
-
 /**
  * Created by colin on 2018/3/22.
  */
 public class ElasticSearch {
+    static String port ="2345";
+    static String host;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         getHelp();
         while (scanner.hasNext()) {
             switch (Integer.parseInt(scanner.next())) {
                 case 1:
-                    try {
                         getElasticInfo();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
                     break;
                 case 2:
-                    try {
-                        createIndexes();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                    createIndexes();
                     break;
                 case 3:
-                    try {
-                        linkRepo();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                    linkRepo();
                     break;
                 case 4:
-                    try {
-                        backup();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                    backup();
                     break;
                 case 5:
-                    try {
-                        getBackupsInfo();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                    getBackupsInfo();
+                    break;
+                case 51:
+                    deleteBackup();
                     break;
                 case 6:
-                    try {
-                        deleteIndexes();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                    deleteIndexes();
+                    break;
+                case 61:
+                    getIndexes();
                     break;
                 case 7:
-                    try {
-                        recoverFromBackup();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                    recoverFromBackup();
+                    break;
+                case 8:
+                    port = scanner.next();
+                    System.out.println(port);
+                    break;
+                case 9:
+                    host = scanner.next();
+                    System.out.println("当前主机"+host);
                     break;
                 case 0:
                     getHelp();
@@ -77,23 +62,24 @@ public class ElasticSearch {
             }
         }
     }
-
-    private static void recoverFromBackup() throws IOException{
-        System.out.println(post("http://localhost:9200/_snapshot/my_backup/snapshot_1/_restore", ""));
+    private static void getIndexes() {
+        System.out.println(get("http://"+host+":"+ port +"/megacorp/employee/1"));
     }
-
-    private static void deleteIndexes() throws IOException{
-
+    private static void deleteBackup() {
+        System.out.println(delete("http://"+host+":" + port + "/_snapshot/my_backup/snapshot_1"));
     }
-
-    private static void getBackupsInfo() throws IOException{
-        System.out.println(get("http://localhost:9200/_snapshot/my_backup/_all"));
+    private static void recoverFromBackup() {
+        System.out.println(post("http://"+host+":"+ port +"/_snapshot/my_backup/snapshot_1/_restore", ""));
     }
-
-    private static void backup() throws IOException {
-        System.out.println(put("http://localhost:9200/_snapshot/my_backup/snapshot_1?wait_for_completion=true", ""));
+    private static void deleteIndexes() {
+        System.out.println(delete("http://"+host+":"+ port +"/megacorp"));
     }
-
+    private static void getBackupsInfo() {
+        System.out.println(get("http://"+host+":"+ port +"/_snapshot/my_backup/_all"));
+    }
+    private static void backup() {
+        System.out.println(put("http://"+host+":"+ port +"/_snapshot/my_backup/snapshot_1?wait_for_completion=true", ""));
+    }
     private static void getHelp() {
         System.out.println("elasticsearch 测试系统");
         System.out.println("请输入您要执行的命令编号：");
@@ -101,39 +87,42 @@ public class ElasticSearch {
         System.out.println("2: 创建一个索引");
         System.out.println("3: 指定仓库");
         System.out.println("4: 备份一个快照");
-        System.out.println("5: 查询快照信息");
-        System.out.println("6: 删除索引");
+        System.out.println("5: 查询快照信息,51: 删除快照");
+        System.out.println("6: 删除索引 ,61: 查询索引");
         System.out.println("7: 从快照中恢复");
+        System.out.println("8: 指定port");
+        System.out.println("9: 指定host");
         System.out.println("0: 获取帮助");
+        System.out.println("请指定端口");
     }
-
-    private static void linkRepo() throws IOException {
-        System.out.println(put("http://localhost:9200/_snapshot/my_backup", "{\n" +
+    private static void linkRepo() {
+        String s = "\\\\\\\\AGENT-55\\\\\\\\backup";
+        if (host.equals("172.30.1.53")) {
+            s = "\\\\\\\\AGENT-53\\\\backup";
+        }
+        System.out.println(put("http://"+host+":"+ port +"/_snapshot/my_backup", "{\n" +
                 "  \"type\": \"fs\",\n" +
                 "  \"settings\": {\n" +
-                "    \"location\": \"/backup/es_backup_origin\"\n" +
+                "    \"location\": \""+s+"\"\n" +
                 "  }\n" +
                 "}"));
+        System.out.println("{\n" +
+                "  \"type\": \"fs\",\n" +
+                        "  \"settings\": {\n" +
+                        "    \"location\": \""+s+"\"\n" +
+                        "  }\n" +
+                        "}");
     }
-
-    private static void createIndexes() throws IOException {
-        System.out.println(post("http://localhost:9200/megacorp/employee/1", "{\n" +
-                "    \"_index\": \"megacorp\",\n" +
-                "    \"_type\": \"employee\",\n" +
-                "    \"_id\": \"1\",\n" +
-                "    \"_version\": 1,\n" +
-                "    \"result\": \"created\",\n" +
-                "    \"_shards\": {\n" +
-                "        \"total\": 2,\n" +
-                "        \"successful\": 1,\n" +
-                "        \"failed\": 0\n" +
-                "    },\n" +
-                "    \"_seq_no\": 0,\n" +
-                "    \"_primary_term\": 1\n" +
+    private static void createIndexes() {
+        System.out.println(post("http://"+host+":"+ port +"/megacorp/employee/1", "{\n" +
+                "    \"first_name\" : \"John\",\n" +
+                "    \"last_name\" :  \"Smith\",\n" +
+                "    \"age\" :        25,\n" +
+                "    \"about\" :      \"I love to go rock climbing\",\n" +
+                "    \"interests\": [ \"sports\", \"music\" ]\n" +
                 "}"));
 
     }
-
     public static String post(String completeUrl, String body) {
         HttpClient httpClient = new DefaultHttpClient();
         HttpPost httpPost = new HttpPost(completeUrl);
@@ -148,7 +137,17 @@ public class ElasticSearch {
             throw new RuntimeException(e);
         }
     }
-
+    public static String delete(String completeUrl) {
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpDelete httpDelete = new HttpDelete(completeUrl);
+        try {
+            httpDelete.getRequestLine();
+            HttpResponse response = httpClient.execute(httpDelete);
+            return getResponse(response);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
     public static String put(String completeUrl, String body) {
         HttpClient httpClient = new DefaultHttpClient();
         HttpPut httpPut = new HttpPut(completeUrl);
@@ -163,8 +162,6 @@ public class ElasticSearch {
             throw new RuntimeException(e);
         }
     }
-
-
     public static String get(String completeUrl) {
         HttpClient httpClient = new DefaultHttpClient();
         HttpGet httpGet = new HttpGet(completeUrl);
@@ -175,7 +172,6 @@ public class ElasticSearch {
             throw new RuntimeException(e);
         }
     }
-
     private static String getResponse(HttpResponse response) throws IOException {
 //        HttpEntity httpEntity = response.getEntity();
 //        BufferedReader bufferedReader = null;
@@ -196,9 +192,8 @@ public class ElasticSearch {
         String json = EntityUtils.toString(response.getEntity());
         return json;
     }
-
-    private static void getElasticInfo() throws IOException {
-//        URL url = new URL("http://localhost:9200/");
+    private static void getElasticInfo()  {
+//        URL url = new URL("http://"+host+":"+port+"/");
 //        URLConnection urlConnection = url.openConnection();                                                    // 打开连接
 //        BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(),"utf-8")); // 获取输入流
 //        String line = null;
@@ -207,7 +202,7 @@ public class ElasticSearch {
 //            sb.append(line + "\n");
 //        }
 //        System.out.println(sb.toString());
-        System.out.println(get("http://localhost:9200/"));
+        System.out.println(get("http://"+host+":"+ port +"/"));
     }
 
 }
